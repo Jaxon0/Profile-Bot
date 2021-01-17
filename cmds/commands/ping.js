ba8afbd93e000cb6cb0ee6a6b90051c4e1b529ac
jaxbots <jobermil05@gmail.com>
2021-01-17 01:32:31 -0600

    Update ping.js

--- cmds/commands/ping.js
+++ cmds/commands/ping.js
@@ -7,7 +7,6 @@ Credits - \`\Developed by jaxon#6666\`]
 $footer[Executed by $username]
 $addTimestamp
 $color[f29cfb]
-$channelSendMessage[784933988648878101;{title:Command Ran}{description:A command has been ran in [$serverName\\]($getServerInvite) by $username}{color:RANDOM}]`
-
+`
 
 }
