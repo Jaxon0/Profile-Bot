07585313f7395ed76732257d9d6be0a91e0ff51e
jaxbots <jobermil05@gmail.com>
2021-01-17 01:34:42 -0600

    Update setbirthday.js

--- cmds/commands/setbirthday.js
+++ cmds/commands/setbirthday.js
@@ -9,6 +9,5 @@ $footer[Executed by $username]
 $addTimestamp
 $color[f29cfb]
 $onlyIf[$charCount<=10;Sorry, birthday character limit is 10!]
-$channelSendMessage[784933988648878101;{title:Command Ran}{description:A command has been ran in [$serverName\\]($getServerInvite) by $username}{color:RANDOM}]
 `
 }
