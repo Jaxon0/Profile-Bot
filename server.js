2c8fd517ce77f2507039b07e07e4b044d6a704fe
jaxbots <jobermil05@gmail.com>
2021-01-16 23:08:30 -0600

    Made server.js self hostable

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const dbd = require("dbd.js")
  
 const bot = new dbd.Bot({
-token: "bot-token", 
-prefix: "p/" 
+token: "bot-token-here", 
+prefix: "prefix-goes-here" 
 })
  
 bot.onMessage()
@@ -24,16 +24,10 @@ code: command.code
 }
 
 bot.status({
-      text: "for p/help",
+      text: "github.com/jaxbots/Profile-Bot",
       type: "WATCHING",
       status: "dnd",
       time: 12
-})
-
-bot.status({
-    text: "https://discord.gg/dYsuMfvZYh",
-    type: "PLAYING",
-    time: 12
 })
 
 bot.variables({
@@ -44,34 +38,15 @@ bot.variables({
     occupation: "Not set yet!",
     birthday: "Not set yet!",
     likes: "0",
-    globalpro: "Not set yet!",
-    globalbio: "Not set yet!",
-    globalname: "Not set yet!",
     avatar: "https://media.discordapp.net/attachments/773055345463394315/776267864931696640/unknown.png?width=453&height=453",
     banner: "https://styleclick.ca/public/front/images/no-img-banner.jpg"
   })
 
-bot.command({
- name: "nuke",
- code: `
-$onlyForIDs[345377106160648203;No.]
- $djsEval[
- d.message.channel.clone()
- .then(c => {
- d.message.channel.delete()
- c.setPosition(d.message.channel.position)
- c.send(d.message.author.tag + ' nuked this channel! https://tenor.com/bko4E.gif')
-
- })
- .catch(() => d.message.channel.send('I dont have enough perms'))
-]
-$onlyPerms[managechannels;{title:Missing permissions!} {description:You are missing the \`MANAGE_CHANNELS\` permission which is needed to run this command} {color:f29cfb}]`
-});
 
 //main page
 bot.command({
 name: "help",
-code: `$reactionCollector[$splitText[1];everyone;1m;🔄,1️⃣,2️⃣,3️⃣,4️⃣;awaitReaction1,awaitReaction2,awaitReaction3,awaitReaction4,awaitReaction5;yes]
+code: `$reactionCollector[$splitText[1];everyone;1m;🔄,1️⃣,2️⃣,3️⃣;awaitReaction1,awaitReaction2,awaitReaction3,awaitReaction4;yes]
 $textSplit[$sendMessage[{title: Help} {description:React to see the following pages:
 1️⃣ - Setup Commands
 2️⃣ - Profile Commands
@@ -133,40 +108,9 @@ $editMessage[$message[1];{footer:🔄 - Return to home page}
 {title:Page 3} {description:**__Other Commands__**
 \`\p/invite\`
 \`\p/upvote\`
-\`\p/ping\`
-
-**Wanna make a suggestion? Run \`\p/suggest (your suggestion)\`**}{color:f29cfb}
+\`\p/ping\`}{color:f29cfb}
 ]
 `
 })
 
-//page 4
-bot.awaitedCommand({
-    name: "awaitReaction5",
-    code: `
-$editMessage[$message[1];{footer:🔄 - Return to home page}
-{title:Page 3} {description:**__Global Profile Commands__**
-\`\p/setglobalbio\` (your bio here)
-\`\p/setglobalname\` (your name here)
-\`\p/setglobalpro\` (your pronouns here)
-
-**Wanna make a suggestion? Run \`\p/suggest (your suggestion)\`**}{color:f29cfb}
-]
-`
 })
-
-bot.botJoinCommand({
-    channel: "$systemChannelID",
-    code:`
-$title[Arrival]
-$description[Thank you so much for inviting me to $serverName!
-
-**What is Profile Bot?**
-Profile Bot is a fun discord bot, that can create basic user profiles for your server!
-
-Invite me [here!\\](https://discord.com/oauth2/authorize?client_id=773408774266028032&permissions=522304&scope=bot)]
-$image[https://i.imgur.com/Yhs6QyY.jpg]
-$footer[Got an idea? Use p/suggest to let my developer know about it!]
-$color[f29cfb]`
-})
-bot.onGuildJoin()
