8aed225a602a8a60edf365a4b50be78f53597a1a
jaxbots <jobermil05@gmail.com>
2021-01-16 23:22:09 -0600

    Delete website.js

--- cmds/commands/website.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "website",
-  code: `
-$description[[Check out our website here!\\](https://www.profile-bot.tk)]
-$color[f29cfb]
-$channelSendMessage[784933988648878101;{title:Command Ran}{description:A command has been ran in [$serverName\\]($getServerInvite) by $username}{color:RANDOM}]
-`
-}
