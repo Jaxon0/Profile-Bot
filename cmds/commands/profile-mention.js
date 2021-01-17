98b09c5e6dc4d1d49eda8b4539a94d40eefec526
jaxbots <jobermil05@gmail.com>
2021-01-17 01:33:00 -0600

    Update profile-mention.js

--- cmds/commands/profile-mention.js
+++ cmds/commands/profile-mention.js
@@ -28,5 +28,5 @@ $image[$getUserVar[banner;$mentioned[1;yes]]]
 $color[f29cfb]
 $footer[Executed by $username]
 $addTimestamp
-$channelSendMessage[784933988648878101;{title:Command Ran}{description:A command has been ran in [$serverName\\]($getServerInvite) by $username}{color:RANDOM}]`
+`
 }
